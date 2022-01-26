1c84f2777a4f3dab8b89b36688abf9001f4e9e30
Revika Mukola <revikanikolaj@gmail.com>
2022-01-26 20:03:36 +0100

    Add Task 1-2

---
+++ js/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+console.group("Task 1");
+
+function pow(base, exponent) {
+  let result = base;
+  let i = 1;
+
+  getPow();
+
+  function getPow() {
+    i++;
+    result *= base;
+    if (i >= exponent) return;
+    getPow();
+  }
+  return result;
+}
+
+console.log("pow() :>> ", pow(5, 5));
+
+console.groupEnd();
+
+console.group("Task 2");
+
+function bracketWrapper(n) {
+  wrapper(n);
+
+  function wrapper(n) {
+    if (n === 0) return;
+    console.log("(");
+    wrapper(n - 1);
+    console.log(")");
+  }
+  return;
+}
+
+bracketWrapper(3);
+
+console.groupEnd();
